acf2785243c9178f9b06c2788e6519c2b867cf6d
Ivan Castro Ruiz <89880933+IvanCastroRuiz@users.noreply.github.com>
2025-10-22 11:40:40 -0500

    Implement custom Cloudinary upload provider

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,22 @@
-export default () => ({});
+import { join } from 'node:path';
+
+const cloudinaryProviderPath = join(__dirname, 'providers', 'cloudinary');
+
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: cloudinaryProviderPath,
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_API_KEY'),
+        api_secret: env('CLOUDINARY_API_SECRET'),
+        default_transformations: env.json('CLOUDINARY_DEFAULT_TRANSFORMATIONS', []),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});

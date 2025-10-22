62400569374458f99b4c1b1463de2180f4e2e00b
Ivan Castro Ruiz <89880933+IvanCastroRuiz@users.noreply.github.com>
2025-10-22 10:49:19 -0500

    Configure Cloudinary upload provider

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,13 @@
-export default () => ({});
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloudName: env('CLOUDINARY_NAME'),
+        apiKey: env('CLOUDINARY_API_KEY'),
+        apiSecret: env('CLOUDINARY_API_SECRET'),
+        defaultTransformations: env.json('CLOUDINARY_DEFAULT_TRANSFORMATIONS', []),
+      },
+    },
+  },
+});
